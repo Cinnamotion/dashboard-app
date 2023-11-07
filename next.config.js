914e8a60dab365c395e0effdc70a4e10e8948fc4
Rigoberto Aravena Miranda <rigoberto.aravena2019@alu.uct.cl>
2023-11-07 13:40:55 -0300

    more

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'dashboard-app-drab-seven.vercel.app',
+			}
+		]
+	}
+};
 
 module.exports = nextConfig;
